d40c24b8e3daac36aff47448e9c738e8d414ed63
Heimir <heimir@i-e46c09aa.greenqloud.internal>
2014-10-18 16:41:06 +0000

    4 done, test for diffrent delimiters

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,11 +3,27 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
+		
+		String criteria = "/" + "/";
 		if(text.equals("")){
 			return 0;
 		}
 		else if(text.contains(",")){
 			return sum(splitNumbers(text));
+		}
+		else if(text.contains(criteria)){
+			String delimiter = text.valueOf(2);
+			String sub = text.substring(4);
+			for(int i = 0; i < sub.length(); i++)
+			{
+				char c = sub.charAt(i);
+				String c2 = Character.toString(c);
+				if(delimiter.equals(c2))
+				{
+					sub = sub.replace(delimiter, ",");
+				}
+			}
+			 return sum(splitNumbers(sub));
 		}
 		else
 			return 1;
